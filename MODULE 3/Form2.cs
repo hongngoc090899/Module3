5879b5bb7fa55a8388296d3c330264469f7f16da
hong ngoc <nguyenthihongngoc090899@gmail.com>
2021-05-05 22:50:01 +0700

    first commit

---
+++ MODULE 3/Form2.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MODULE_3
+{
+    public partial class Form2 : Form
+    {
+        CSDLDataContext db = new CSDLDataContext();
+
+        public static Schedule schedule = null;
+        public static List<DataGridViewRow> dsHanhKhach = new List<DataGridViewRow>();
+        public Form2()
+        {
+            InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (txt_firstName.Text == "" || txt_lastName.Text == "" || txt_passportNumber.Text == ""
+            || dt_birthDate.Text == "" || txt_phone.Text == "" || cb_passportCountry.Text == "")
+            {
+                MessageBox.Show("Bạn chưa điền đủ thông tin", "Cảnh báo!");
+                return;
+            }
+            DataGridViewRow dongmoi = (DataGridViewRow)dgv_passengersList.Rows[0].Clone();
+            dongmoi.Cells[0].Value = txt_firstName.Text;
+            dongmoi.Cells[1].Value = txt_lastName.Text;
+            dongmoi.Cells[2].Value = dt_birthDate.Text;
+            dongmoi.Cells[3].Value = txt_passportNumber.Text;
+            dongmoi.Cells[4].Value = cb_passportCountry.Text;
+            dongmoi.Cells[5].Value = txt_phone.Text;
+            if (dgv_passengersList.Rows.Count - 1 == Form1.soKH)
+            {
+                MessageBox.Show("Bạn đã đặt đủ số lượng vé", "Thông báo");
+                return;
+            }
+            for (int i = 0; i < dgv_passengersList.Rows.Count - 1; i++)
+            {
+                if (dongmoi.Cells[3].Value.ToString() == dgv_passengersList.Rows[i].Cells[3].Value.ToString())
+                {
+                    MessageBox.Show("đã tồn tại passport này !", "ERROR");
+                    return;
+                }
+            }
+            dgv_passengersList.Rows.Add(dongmoi);
+            return;
+        }
+
+      
+        private void cb_passportCountry_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void taoCombobox()
+        {
+            var contries = from x in db.Countries select x;
+            cb_passportCountry.ValueMember = "name";
+            cb_passportCountry.DataSource = contries;
+            
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            taoCombobox();
+            this.CenterToParent();
+            List<DataGridViewRow> listRow = new List<DataGridViewRow>();
+            listRow = (List<DataGridViewRow>)this.Tag;
+            DataGridViewRow d1 = new DataGridViewRow();
+            DataGridViewRow d2 = new DataGridViewRow();
+
+            d1 = listRow[0];
+            d2 = listRow[1];
+            lb_from_outbound.Text = d1.Cells[0].Value.ToString();
+            lb_to_outbound.Text = d1.Cells[1].Value.ToString();
+            lb_cabintype_outbound.Text = Form1.cabinn;
+            lb_date_outbound.Text = d1.Cells[2].Value.ToString();
+            lb_flightnumber_outbound.Text = d1.Cells[4].Value.ToString();
+            gr_return.Visible = false;
+            if (Form1.checkOneWay == false)
+            {
+                lb_from_return.Text = d2.Cells[0].Value.ToString();
+                lb_to_return.Text = d2.Cells[1].Value.ToString();
+                lb_cabintype_return.Text = Form1.cabinn;
+                lb_date_return.Text = d2.Cells[2].Value.ToString();
+                lb_flightnumber_return.Text = d2.Cells[4].Value.ToString();
+                gr_return.Visible = true;
+            }
+        }
+        public static List<List<string>> list_passenger_dgv = new List<List<string>>();
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (dgv_passengersList.Rows.Count - 1 != Form1.soKH)
+            {
+                MessageBox.Show("chưa đủ số lượng vé, mời đặt thêm");
+                return;
+            }
+            dsHanhKhach.Clear();
+            for (int i = 0; i < dgv_passengersList.Rows.Count - 1; i++)
+                dsHanhKhach.Add((DataGridViewRow)dgv_passengersList.Rows[i]);
+            Form3 myform = new Form3();
+            myform.ShowDialog();
+
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int xoa = dgv_passengersList.CurrentRow.Index;
+            dgv_passengersList.Rows.RemoveAt(xoa);
+            clear_text();
+        }
+        private void clear_text()
+        {
+            txt_firstName.Clear();
+            txt_lastName.Clear();
+            txt_passportNumber.Clear();
+            txt_phone.Clear();
+        }
+        private void dgv_passengersList_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            
+        }
+
+        private void groupBox3_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ MODULE 3/Form3.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MODULE_3
+{
+    public partial class Form3 : Form
+    {
+        CSDLDataContext db = new CSDLDataContext();
+       // List<DataGridViewRow> ds = new List<DataGridViewRow>();
+        public Form3()
+        {
+            InitializeComponent();
+        }
+
+        void loadData()
+        {
+            this.CenterToScreen();
+            List<DataGridViewRow> ds = new List<DataGridViewRow>();
+            ds = Form1.dongMoi;
+            DataGridViewRow d1 = new DataGridViewRow();
+            DataGridViewRow d2 = new DataGridViewRow();
+
+            d1 = ds[0];
+            string t1 = d1.Cells[5].Value.ToString();
+            int t = int.Parse(t1.Replace("$", "").Replace(",", ""));
+            if (Form1.checkOneWay == false)
+            {
+                d2 = ds[1];
+                string t2 = d2.Cells[5].Value.ToString();
+                t += int.Parse(t2.Replace("$", "").Replace(",", ""));
+            }
+            lable_total_amount.Text = (t * Form1.soKH).ToString("C0");
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form3_Load(object sender, EventArgs e)
+        {
+            loadData();
+           
+        }
+
+        //lay ngau nhien 1 ki tu 
+        public Random r = new Random();
+        public string layKiTu()
+        {
+            string chuoi = "qwertyuiopasdfghjklzxcvbnmQWERTYUIOPASDFGHJKLZXCVBNM0123456789";
+            string kiTu = "";
+            for (int i = 0; i < 6; i++)
+                kiTu += chuoi[r.Next(0, chuoi.Length - 1)];
+            return kiTu;
+
+        }
+        List<Ticket> dsVe = new List<Ticket>();
+        void themVe(string chuoi, List<DataGridViewRow> dsKhach, DateTime dt, string soHieu)
+        {
+
+            for (int i = 0; i < dsKhach.Count; i++)
+            {
+                Ticket ve = new Ticket();
+                ve.UserID = 1;
+                //lay so hieu chuyen bay
+                ve.ScheduleID = db.Schedules.Where(p => p.Date >= dt && p.FlightNumber == soHieu).Select(p => p.ID).FirstOrDefault();
+                ve.CabinTypeID = db.CabinTypes.Where(p => p.Name == Form1.cabinn).Select(p => p.ID).FirstOrDefault();
+                ve.BookingReference = chuoi;
+                ve.Firstname = dsKhach[i].Cells[0].Value.ToString();
+                ve.Lastname = dsKhach[i].Cells[1].Value.ToString();
+                ve.Email = null;
+                ve.PassportNumber = dsKhach[i].Cells[3].Value.ToString();
+                string a = dsKhach[i].Cells[4].Value.ToString();
+                ve.PassportCountryID = db.Countries.Where(p => p.Name == a).Select(p => p.ID).FirstOrDefault();
+                ve.Phone = dsKhach[i].Cells[5].Value.ToString();
+                ve.Confirmed = true;
+                dsVe.Add(ve);
+            }
+        }
+
+
+        private void btn_Issue_tickets_Click(object sender, EventArgs e)
+        {
+
+            string chuoi = "";
+            //kiem tra da ton tai chuoi ki tu hay chua
+            while (true)
+            {
+                chuoi = layKiTu();
+                var data = db.Tickets.Where(p => p.BookingReference == chuoi).ToList();
+                if (data.Count == 0)
+                    break;
+            }
+            List<DataGridViewRow> dsKhach = new List<DataGridViewRow>();
+            dsKhach = Form2.dsHanhKhach;
+            List<DataGridViewRow> dsMayBay = new List<DataGridViewRow>();
+            dsMayBay = Form1.dongMoi;
+            var d1 = dsMayBay[0];
+
+            string date = d1.Cells[2].Value.ToString();
+            DateTime dt = DateTime.ParseExact(date, "MM/dd/yyyy", null);
+            //12-32-32 -> [12, 32, 32]
+            string[] s = d1.Cells[4].Value.ToString().Split('-');
+            foreach (var soHieu in s)
+                themVe(chuoi, dsKhach, dt, soHieu);
+            if (Form1.checkOneWay == false)
+            {
+                var d2 = dsMayBay[1];
+                string[] s1 = d2.Cells[4].Value.ToString().Split('-');
+                foreach (var soHieu in s1)
+                    themVe(chuoi, dsKhach, dt, soHieu);
+            }
+            db.Tickets.InsertAllOnSubmit(dsVe);
+            db.SubmitChanges();
+            MessageBox.Show("Đặt vé thành công", "Thông báo");
+        }
+    }
+}
